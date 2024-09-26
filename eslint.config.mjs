98dbe245bf70c091e189b0d6c8424abcdb28f431
Bahriddinov Muhammad <salahbm@momenti.biz>
2024-09-26 14:06:41 +0900

    fix: lint

---
+++ eslint.config.mjs
@@ -0,0 +1,107 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+import _import from 'eslint-plugin-import';
+import { FlatCompat } from '@eslint/eslintrc';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+const config = [
+  {
+    ignores: [
+      '**/build',
+      '**/dist',
+      '**/public',
+      '**/.next',
+      '**/.cache',
+      '**/package-lock.json',
+      '**/public',
+      '**/node_modules',
+      '**/next-env.d.ts',
+      '**/next.config.ts',
+      '**/yarn.lock',
+    ],
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'next/core-web-vitals',
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      react,
+      prettier,
+      import: _import,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      sourceType: 'module',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        parser: tsParser,
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: './tsconfig.json',
+      },
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'no-duplicate-imports': 'error',
+      'react/jsx-filename-extension': [
+        'warn',
+        {
+          extensions: ['.tsx'],
+        },
+      ],
+      '@typescript-eslint/camelcase': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@next/next/no-duplicate-head': 'off',
+      'no-console': 'error',
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          ts: 'never',
+          tsx: 'never',
+          js: 'never',
+          jsx: 'never',
+        },
+      ],
+      'prettier/prettier': [
+        'warn',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  },
+];
+
+export default config;
