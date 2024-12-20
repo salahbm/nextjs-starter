c0cc97ed26cdc218a131091de937c67a9932231f
Bahriddinov Muhammad <salahbm@momenti.biz>
2024-12-20 14:10:20 +0900

    style(home): home page is updated to give better explaination and links
    This commit contains updated home page and its test case

--- __test__/home.test.tsx
+++ __test__/home.test.tsx
@@ -3,9 +3,37 @@ import { render, screen } from '@testing-library/react';
 import Home from '@/app/[locale]/page';
 
 describe('Home Component', () => {
-  it('renders the Home component with correct content', () => {
+  it('renders the Home component with correct headings', () => {
     render(<Home />);
-    const headingElement = screen.getByText(/Home/i);
-    expect(headingElement).toBeInTheDocument();
+
+    // Check if the main heading is rendered
+    const mainHeading = screen.getByText(/Next JS Template/i);
+    expect(mainHeading).toBeInTheDocument();
+
+    // Check if the subheading is rendered
+    const subHeading = screen.getByText(
+      /This is template is developed for Next JS Projects as a starting point/i,
+    );
+    expect(subHeading).toBeInTheDocument();
+
+    // Check if the README link is rendered
+    const readmeLink = screen.getByRole('link', {
+      name: /Check out the README/i,
+    });
+    expect(readmeLink).toBeInTheDocument();
+    expect(readmeLink).toHaveAttribute(
+      'href',
+      'https://gitlab.com/momenti/nextjs-boilerplate-by-salah/-/blob/main/README.md',
+    );
+
+    // Check if the ESLint link is rendered
+    const eslintLink = screen.getByRole('link', {
+      name: /This Project uses Momenti ESLint/i,
+    });
+    expect(eslintLink).toBeInTheDocument();
+    expect(eslintLink).toHaveAttribute(
+      'href',
+      'https://gitlab.com/momenti/momenti-eslint',
+    );
   });
 });
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,31 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Home = (): JSX.Element => {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <h1 className="text-3xl font-bold">Home</h1>
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-5">
+      <h1 className="text-3xl font-bold text-rose-400">Next JS Template</h1>
+      <h2 className="text-xl text-neutral-700">
+        This is template is developed for Next JS Projects as a starting point
+      </h2>
+
+      {/* CHECK OUT THE README */}
+      <Link
+        href="https://gitlab.com/momenti/nextjs-boilerplate-by-salah/-/blob/main/README.md"
+        target="_blank"
+        className="font-body-1 text-gray-6 underline"
+      >
+        Check out the README
+      </Link>
+
+      {/* CHECK OUT THE MOMENTI-ESLINT */}
+      <Link
+        href="https://gitlab.com/momenti/momenti-eslint"
+        target="_blank"
+        className="font-body-2 text-gray-7 underline"
+      >
+        This Project uses Momenti ESLint
+      </Link>
     </div>
   );
 };
